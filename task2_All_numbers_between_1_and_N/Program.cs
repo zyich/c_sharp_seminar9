2edaeda3b1741379e56a276f0b6246a7465a7fbc
zyich <zuilinho@yandex.ru>
2023-07-19 14:05:53 +0200

    task 2 all number from 1 to N

---
+++ task2_All_numbers_between_1_and_N/Program.cs
@@ -0,0 +1,13 @@
+﻿/* 
+Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N. 
+N = 5 -> "1, 2, 3, 4, 5" 
+N = 6 -> "1, 2, 3, 4, 5, 6" 
+*/ 
+ 
+string ReturnRealDigits(int startNum, int number)
+    { 
+        if(startNum == number) return startNum.ToString(); 
+        return startNum + " " + ReturnRealDigits(startNum+1,number); // " " для преобразования всего рядом в string 
+    } 
+
+Console.WriteLine(ReturnRealDigits(1,5));
