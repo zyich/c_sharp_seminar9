e8ffae6d403f9647ac4cd60ac7e71f0b72290809
zyich <zuilinho@yandex.ru>
2023-07-20 13:58:31 +0200

    task 4 A power B

---
+++ task4_A_power_B/Program.cs
@@ -0,0 +1,15 @@
+﻿/* 
+Задача 69: Напишите программу, которая 
+на вход принимает два числа A и B, 
+и возводит число А в целую 
+степень B с помощью рекурсии. 
+A = 3; B = 5 -> 243 (3⁵) 
+A = 2; B = 3 -> 8 
+*/ 
+ 
+int GetPowNumber (int number, int pow){ 
+    if(pow == 0) return 1;  
+        return number * GetPowNumber(number, pow-1); 
+} 
+Console.Clear(); 
+Console.WriteLine(GetPowNumber(3,5));
